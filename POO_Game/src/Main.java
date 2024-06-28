d34e4816ab293d28b528ee02f37d518733f9daa3
invokus <jhermanfcs97@hotmail.com>
2024-06-27 20:52:53 -0500

    update desde Intellij editado por

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        //EDITADO POR JHERMAN CANACUAN
+
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
